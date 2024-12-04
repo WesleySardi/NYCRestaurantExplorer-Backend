9683fadf8c10e78e916f82a20da6d59d69a07f80
Wesley Sardi <wesley.sardi@gmail.com>
2024-12-04 17:49:54 -0300

    First commit

---
+++ src/main/java/org/example/nycrestaurantexplorer/NycrestaurantexplorerApplication.java
@@ -0,0 +1,13 @@
+package org.example.nycrestaurantexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NycrestaurantexplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NycrestaurantexplorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nycrestaurantexplorer/NycrestaurantexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nycrestaurantexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NycrestaurantexplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
