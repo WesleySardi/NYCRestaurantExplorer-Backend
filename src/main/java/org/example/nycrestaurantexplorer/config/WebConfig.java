866f15b369cbbb881298771def7f7e1d0d8ef613
Wesley Sardi <wesley.sardi@gmail.com>
2024-12-17 19:44:22 -0300

    Last changes.

--- src/main/java/org/example/nycrestaurantexplorer/config/WebConfig.java
+++ src/main/java/org/example/nycrestaurantexplorer/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080")
+                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "https://nyc-restaurant-explorer-frontend.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
