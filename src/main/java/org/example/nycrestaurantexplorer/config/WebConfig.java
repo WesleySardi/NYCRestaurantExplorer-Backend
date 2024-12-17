ec1f41e70b1fe9d3f9b01613b953a2d917351ea5
Wesley Sardi <wesley.sardi@gmail.com>
2024-12-17 20:27:25 -0300

    Fixes.

--- src/main/java/org/example/nycrestaurantexplorer/config/WebConfig.java
+++ src/main/java/org/example/nycrestaurantexplorer/config/WebConfig.java
@@ -8,11 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8080", "https://nyc-restaurant-explorer-frontend.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**").
+                allowedMethods("*")
+                .allowedOrigins("*");
     }
 }
 
